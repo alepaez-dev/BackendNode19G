e810e7ac809d17ca32887d4ef72fd16c1e663515
alepaez-dev <alepaezdev@gmail.com>
2022-06-27 20:31:20 -0500

    feat: ejercicio

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Hacer una funcion que reciba como parametro un nombre, y me imprima, Hola {nombre}, como estan ?
+ */
+
+/**
+ * No se manipula la DOM
+ * No se manipula la BOM
+ */
+
+console.log("hello koders")
+
+function saludar(nombre) {
+  return `Hola ${ nombre} como estas`;
+}
+
+const resultadoSaludar = saludar("Bere")
+
+/**
+ * Separar del arreglo nombres los que tienen < de 5 letras y > 5 letras
+ */
+
+const nombres = [
+  "Paco",
+  "Lorena",
+  "Kike",
+  "Abraham",
+  "Cris",
+  "Victor",
+  "Marco",
+  "Israel",
+  "Annie",
+];
