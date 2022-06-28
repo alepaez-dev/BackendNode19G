6c851b3841aca4ce7d8ae4142d316db474ed1769
alepaez-dev <alepaezdev@gmail.com>
2022-06-27 21:30:56 -0500

    buscador.js

---
+++ clase2/buscador.js
@@ -0,0 +1,27 @@
+// Process
+if (process.argv[3] !== undefined) {
+  console.log(process.argv[3])
+}
+
+const nombres = [
+  "Paco",
+  "Lorena",
+  "Kike",
+  "Abraham",
+  "Cris",
+  "Victor",
+  "Marco",
+  "Israel",
+  "Annie",
+  "Ërnesto",
+  "Francisco",
+  "Lenny",
+  "Rod",
+  "Noemi",
+  "Benjamin",
+];
+
+/**
+ * Generar un script que reciba un parametro y nos diga si el nombre esta en el arreglo nombres. Si si esta
+ * que nos imprima si estas. Y si no, que imprima Afuera de aqui
+ */
