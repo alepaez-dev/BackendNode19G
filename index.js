4bacee1559e61787ffacef0529279f1ea71c1a8c
alepaez-dev <alepaezdev@gmail.com>
2022-06-27 21:00:56 -0500

    destructuracion

--- index.js
+++ index.js
@@ -30,3 +30,39 @@ const nombres = [
   "Israel",
   "Annie",
 ];
+
+const nombresMenores = nombres.filter(nombre => {
+  if(nombre.length < 5) {
+    return nombre
+  }
+})
+
+const nombresMenores2 = nombres.filter(nombre => nombre.length < 5);
+const nombresMayores2 = nombres.filter(nombre => nombre.length >= 5);
+// console.log(nombresMenores2)
+// console.log(nombresMayores2)
+
+
+const separar = () => {
+  let nombresMenoresArray = []
+  let nombresMayoresArray = []
+
+  nombres.forEach(nombre => {
+   
+    if(nombre.length < 5) {
+      nombresMenoresArray.push(nombre);
+    } else {
+      nombresMayoresArray.push(nombre);
+    }
+  })
+
+  // Destructuracion
+  return [nombresMenoresArray, nombresMayoresArray]
+}
+
+// Destructuracion
+const [nombresMenoresArray, nombresMayoresArray] = separar()
+
+const resultado = separar()
+console.log("resultado", resultado[0])
+console.log("resultado", resultado[1])
